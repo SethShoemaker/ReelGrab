6df36d17c04ad8fa3a6f1dc228df00be5b6800b0
seth shoemaker <contact@sethshoemaker.com>
2025-01-18 18:05:58 +0000

    fix dev container connection issue

--- src/ReelGrab.Web/Program.cs
+++ src/ReelGrab.Web/Program.cs
@@ -10,7 +10,11 @@ builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.Urls.Add("http://*:5242");
+} 
+else
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
