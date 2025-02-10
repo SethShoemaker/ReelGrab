87420bb0c28fc744afdb2a257ad3c9f13b2a0118
seth shoemaker <contact@sethshoemaker.com>
2025-02-10 23:22:19 +0000

    add get torrent hash by magnet link util

--- src/Utils/Torrents.cs
+++ src/Utils/Torrents.cs
@@ -1,6 +1,8 @@
+using System.Text.RegularExpressions;
+
 namespace ReelGrab.Utils;
 
-public static class Torrents
+public static partial class Torrents
 {
     public static async Task<string> GetTorrentHashByUrlAsync(string torrentUrl)
     {
@@ -17,6 +19,19 @@ public static class Torrents
             throw new Exception($"error getting hash for torrent {torrentUrl}");
         }
         return show[beg..end];
+    }
+
+    [GeneratedRegex(@"xt=urn:btih:([a-fA-F0-9]{40}|[A-Z2-7]{32})")]
+    private static partial Regex MagnetInfoHash();
+
+    public static Task<string> GetTorrentHashByMagnetLinkAsync(string magnetLink)
+    {
+        var match = MagnetInfoHash().Match(magnetLink);
+        if(!match.Success)
+        {
+            throw new Exception($"{magnetLink} is not a valid magnet link");
+        }
+        return Task.FromResult(match.Groups[1].Value);
     }
 
     private static async Task<string> RunTorrentShowAsync(string torrentUrl)
