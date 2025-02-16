59306b78fada5ce6099dbd180b8065d1ac86122d
seth shoemaker <contact@sethshoemaker.com>
2025-02-16 02:16:49 +0000

    edit page title styles, add non functional media search

--- frontend/src/app/components/media-search/media-search.component.ts
+++ frontend/src/app/components/media-search/media-search.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ApiService } from '../../services/api/api.service';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, Subscription } from 'rxjs';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-media-search',
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf, NgFor],
   templateUrl: './media-search.component.html',
   styleUrl: './media-search.component.scss'
 })
-export class MediaSearchComponent {
+export class MediaSearchComponent implements OnDestroy {
 
+  searchControl = new FormControl()
+  searchControlSub: Subscription;
+  searchingSub: Subscription | null = null;
+  searchResults: any;
+  loading = false;
+
+  constructor(public api: ApiService) {
+    this.searchControlSub = this.searchControl.valueChanges
+      .pipe(debounceTime(1000))
+      .subscribe(value => {
+        this.searchingSub?.unsubscribe();
+        if (value.length == 0) {
+          this.loading = false;
+          this.searchResults = [];
+          return;
+        }
+        this.loading = true;
+        this.searchingSub = this.api.mediaSearch(value).subscribe(results => {
+          this.searchResults = results;
+          this.loading = false;
+        })
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.searchControlSub.unsubscribe()
+    this.searchingSub?.unsubscribe()
+  }
 }
--- frontend/src/app/services/api/api.service.ts
+++ frontend/src/app/services/api/api.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  mediaSearch(query: string): Observable<any> {
+    return this.http.get(`http://localhost:5242/media_index/search?query=${query}`)
+  }
 }
